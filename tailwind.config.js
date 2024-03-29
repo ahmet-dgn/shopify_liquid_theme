a6c55e6c78289337ba3fe5f3eb240f442f65e98b
ahmet-dgn <ahmetcandgn@gmail.com>
2024-03-29 23:54:32 +0300

    Product page carousel added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/customers/*.liquid',
-    './templates/*.liquid',
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/customers/*.liquid",
+    "./templates/*.liquid",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-
-  ],
-}
-
+  plugins: [],
+};
